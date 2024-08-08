fe8288bd4dc3e1011f4c0a62adbe1b89707c5d5d
dearxue.com <a@dearxue.com>
2024-08-09 07:50:38 +0800

    make it easyier

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Featured() {
       href="/docs/changelog/sdk"
       className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden xl:flex items-center hover:bg-sky-400/20"
     >
-      <strong className="font-semibold">SDK v1.0.2 + Plugin 0.5.0</strong>
+      <strong className="font-semibold">SDK v1.0.4 + Plugin 0.5.1</strong>
       <svg
         width="2"
         height="2"
