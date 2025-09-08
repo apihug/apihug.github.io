5cfe6fe115a406f55f3dd72e0fae8718cfc30bc8
apihug <ak@apihug.com>
2025-09-08 09:19:42 +0800

    polish the doc

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Featured() {
       href="/docs/changelog/sdk.html"
       className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden xl:flex items-center hover:bg-sky-400/20"
     >
-      <strong className="font-semibold">SDK v1.4.2 + Plugin 0.7.6</strong>
+      <strong className="font-semibold">SDK v1.4.3 + Plugin 0.7.7</strong>
       <svg
         width="2"
         height="2"
@@ -180,6 +180,11 @@ export function NavItems() {
       <li>
         <Link href="/docs/kola" className="hover:text-sky-500 dark:hover:text-sky-400" title={'Api Test'}>
           Kola
+        </Link>
+      </li>
+      <li>
+        <Link href="https://apihug.github.io" className="hover:text-sky-500 dark:hover:text-sky-400" title={'Permanent Homepage'}>
+          Home
         </Link>
       </li>
     </>
