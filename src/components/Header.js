6cb0ba97c953a26f3c62b49d1dc724e3cc5658a1
apihug <ak@apihug.com>
2025-09-09 08:31:24 +0800

    for the 1.4.5-RELEASE

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Featured() {
       href="/docs/changelog/sdk.html"
       className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden xl:flex items-center hover:bg-sky-400/20"
     >
-      <strong className="font-semibold">SDK v1.4.3 + Plugin 0.7.7</strong>
+      <strong className="font-semibold">SDK v1.4.5 + Plugin 0.7.7</strong>
       <svg
         width="2"
         height="2"
